f567d492380ab0a418acc2080eb587d24bde1c4a
jamalkaksouri <jamal.kaksouri@gmail.com>
2022-01-16 14:53:51 +0330

    popup added

--- script.js
+++ script.js
@@ -26,3 +26,21 @@ if (overlay) {
     document.body.style.overflowY = "visible";
   });
 }
+
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
