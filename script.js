a7863dac59cd8cd86e674eacaf54c5a1b298dad1
jamalkaksouri <jamal.kaksouri@gmail.com>
2022-01-18 17:26:18 +0330

    scrollup added

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+import smoothscroll from "smoothscroll-polyfill";
+
+window.__forceSmoothScrollPolyfill__ = true;
+
 const bar = document.querySelector("#bar");
 const nav = document.querySelector("#navbar");
 const close = document.querySelector("#close");
